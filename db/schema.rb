a689f63c0c2ddbe47b9befcdb19e853a78c837bd
WozniakMac <maciekwo@gmail.com>
2015-05-10 17:43:10 +0200

    Votes model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150510153826) do
+ActiveRecord::Schema.define(version: 20150510154231) do
 
   create_table "boxes", force: :cascade do |t|
     t.integer  "user_id"
@@ -63,5 +63,16 @@ ActiveRecord::Schema.define(version: 20150510153826) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "idea_id"
+    t.integer  "user_id"
+    t.boolean  "rate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["idea_id"], name: "index_votes_on_idea_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end

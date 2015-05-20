baebc53d16534901de373357eb51f22e5f8c6e81
WozniakMac <maciekwo@gmail.com>
2015-05-20 23:41:57 +0200

    Add status to idea

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -3,4 +3,12 @@ class Idea < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_many :votes
+
+  def set_status(status)
+  	self.status = case status
+	  	when :new then 1
+	  	when :planed then 2
+	  	when :inprogress then 3
+	  	when :complited then 4
+  	end
 end

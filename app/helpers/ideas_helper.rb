3efe76c140415dda0da0165f77d33c7f4c59f9da
Maciej Woźniak <maciekwo@gmail.com>
2016-02-09 19:42:32 +0100

    Change opacity on likes buttons when clicked

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,2 +1,18 @@
 module IdeasHelper
+  def idea_button_class(idea, status)
+    if liked?(idea) and status == :liked
+      return "ids-active"
+    elsif disliked?(idea) and status == :disliked
+      return "ids-active"
+    end
+    return ""
+  end
+
+  def liked?(idea)
+    idea.votes.where(rate: true).where(user: current_user).count > 0
+  end
+
+  def disliked?(idea)
+    idea.votes.where(rate: false).where(user: current_user).count > 0
+  end
 end
